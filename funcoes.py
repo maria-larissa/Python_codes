d372a5d4439cb7f2bda0c25949467a85f0b10c01
larissa <>
2022-10-10 15:44:52 -0300

    Atualização q1.py e adição do funções.py.

---
+++ funcoes.py
@@ -0,0 +1,6 @@
+#estrutura de funcoes em Python
+#Retorna os dados em um estrutura tupla
+def velocidade(espaco,tempo):
+    return espaco/tempo,(espaco/tempo)/tempo
+
+print(velocidade(10,2))
--- lista1/q1.py
+++ lista1/q1.py
@@ -1,15 +1,19 @@
 list=(12,2,4,8,29,45,78,36,-17,2,12,8,3,3,-52)
+soma=0
 for i in range(0, len(list)-1):
+    soma=soma+list[i]
+    if(i == len(list))-1:
+        media=(float)(soma/(len(list)))
     if i==0:
         max_value=list[i]
         min_value=list[i]
-        
     else:
         if list[i]>max_value:
             max_value=list[i]
         if list[i]<min_value:
             min_value=list[i]
-
+    
+print(soma)
 print("a) O maior elemento da lista é {}.".format(max_value))
 print("b) O menor elemento da lista é {}.".format(min_value))
-print("c) O maior elemento da lista é {}.".format(max_value))
+print("c) A média dos elementos da lista é {}.".format(media))
