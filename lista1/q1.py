fd0abc5d3ea0dcdf63b76320dca06a98f987a596
larissa <larissa@debian-BULLSEYE-live-builder-AMD64>
2022-10-11 00:49:17 -0300

    Atualização lista1/q1.py

--- lista1/q1.py
+++ lista1/q1.py
@@ -1,19 +1,43 @@
-list=(12,2,4,8,29,45,78,36,-17,2,12,8,3,3,-52)
-soma=0
-for i in range(0, len(list)-1):
-    soma=soma+list[i]
-    if(i == len(list))-1:
-        media=(float)(soma/(len(list)))
-    if i==0:
-        max_value=list[i]
-        min_value=list[i]
-    else:
-        if list[i]>max_value:
-            max_value=list[i]
-        if list[i]<min_value:
-            min_value=list[i]
+list = (12,2,4,8,29,45,78,36,-17,2,12,8,3,3,-52)
+list_pares = []
+soma_neg = 0
+media = 0
+soma = 0
+
+for i in range(0, len(list)):
+    soma = soma + list[i]
+
+    #cria lista só com os números pares
+    if list[i] % 2 == 0:
+        list_pares.append(list[i])
     
-print(soma)
-print("a) O maior elemento da lista é {}.".format(max_value))
-print("b) O menor elemento da lista é {}.".format(min_value))
-print("c) A média dos elementos da lista é {}.".format(media))
+    #soma números negativos
+    if list[i] < 0:
+        soma_neg = soma_neg + list[i]
+
+    #defini valores iniciais para max e min e cria contador para 1º elemento.
+    if i == 0:
+        cont = 1
+        max_value = list[i]
+        min_value = list[i]
+    
+    #atualiza valores max, min e cont.
+    else:
+        if list[i] == list[0]:
+            cont = cont+1
+        if list[i] > max_value:
+            max_value = list[i]
+        if list[i] < min_value:
+            min_value = list[i]
+    
+    #calcula média
+    if i == (len(list)-1):
+        media = (float)(soma/(len(list)))
+    
+    
+print("a - O maior elemento da lista é {}.".format(max_value))
+print("b - O menor elemento da lista é {}.".format(min_value))
+print("c - Os elementos pares da lista são {}.".format(list_pares))
+print("d - O 1º elementos da lista aperce na lista {} vezes.".format(cont))
+print("e - A média dos elementos da lista é {:.2f}.".format(media))
+print("f - A soma dos elementos negativos da lista é {}.".format(soma_neg))
